3a8bcfef78dc038d4726431653d4217cec502f42
CristopherAreche <cristopherareche764@gmail.com>
2023-02-07 22:03:00 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchBar } from "./SearchBar.jsx";
+export { default as SideBar } from "./SideBar.jsx";
+export { default as Videos } from "./Videos.jsx";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
